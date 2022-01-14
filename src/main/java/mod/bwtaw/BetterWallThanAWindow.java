a5ed96db1f601607220eb0b6c0da1c52ee03ac80
Linguardium <linguardium@hobbyist.dev>
2022-01-14 14:05:28 -0800

    actually include the 1.18 updated files

--- src/main/java/mod/bwtaw/BetterWallThanAWindow.java
+++ src/main/java/mod/bwtaw/BetterWallThanAWindow.java
@@ -3,12 +3,13 @@ package mod.bwtaw;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.LiteralText;
-import org.lwjgl.input.Keyboard;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class BetterWallThanAWindow implements ClientModInitializer {
@@ -18,13 +19,14 @@ public class BetterWallThanAWindow implements ClientModInitializer {
 	public void onInitializeClient() {
 		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
 				"key.bwtaw.hide", // The translation key of the keybinding's name
-				Keyboard.KEY_F12,
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_F12,
 				"key.category.bwtaw" // The translation key of the keybinding's category.
 		));
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (keyBinding.wasPressed()) {
 				hidePlayers=!hidePlayers;
-					client.player.sendMessage(new LiteralText("Players are now "+ ((hidePlayers)?"Hidden":"Shown")));
+					client.player.sendMessage(new LiteralText("Players are now "+ ((hidePlayers)?"Hidden":"Shown")),false);
 			}
 		});
 	}
--- src/main/java/mod/bwtaw/mixin/PlayerRendererMixin.java
+++ src/main/java/mod/bwtaw/mixin/PlayerRendererMixin.java
@@ -2,7 +2,10 @@ package mod.bwtaw.mixin;
 
 import mod.bwtaw.BetterWallThanAWindow;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +14,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerEntityRenderer.class)
 public class PlayerRendererMixin {
     @Inject(at=@At("HEAD"), method="render", cancellable = true)
-    private void hidePlayers(AbstractClientPlayerEntity abstractClientPlayerEntity, double d, double e, double f, float g, float h, CallbackInfo info) {
+    private void hidePlayers(AbstractClientPlayerEntity abstractClientPlayerEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo info) {
+        if (BetterWallThanAWindow.hidePlayers) {
+            info.cancel();
+        }
+    }
+    @Inject(at=@At("HEAD"), method="renderLabelIfPresent", cancellable = true)
+    protected void hideLabel(AbstractClientPlayerEntity abstractClientPlayerEntity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo info) {
         if (BetterWallThanAWindow.hidePlayers) {
             info.cancel();
         }
